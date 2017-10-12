4971fe7e5139e2bc71f48756af4e8d5e65b6ab89
Mgen <m@mgenware.com>
2017-10-12 14:15:29 +0800

    Add an example

---
+++ example/example.js
@@ -0,0 +1,11 @@
+// Node.js 8+
+const fx43 = require('..');
+
+async function printChangedFiles() {
+  const files = await fx43.start('.', '**/*.html', './.cache');
+  console.log(`${files.length} file(s) changed.\n${files}`);
+}
+
+(async () => {
+  await printChangedFiles();
+})();
