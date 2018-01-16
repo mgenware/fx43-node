7dc1c0f3a4fe51bc3993078afb238ba1dfca9481
Mgen <m@mgenware.com>
2018-01-16 10:53:45 +0800

    Test against both relative and absolute paths

--- tests/main-ignore-file.test.ts
+++ tests/main-ignore-file.test.ts
@@ -24,51 +24,50 @@ async function touchAsync(path: string) {
 }
 
 const dataDir = nodepath.join(__dirname, 'data');
-let tmpDir = '';
-let cacheDir = '';
 
-beforeAll(() => {
-  tmpDir = newTmpDir();
+function testWithTmpDir(tmpDir: string, cacheDir: string, title: string) {
   copyDir.sync(dataDir, tmpDir);
-  cacheDir = newTmpDir();
   // tslint:disable-next-line: no-console
   console.log(`Data  dir: ${tmpDir}`);
   // tslint:disable-next-line: no-console
   console.log(`Cache dir: ${cacheDir}`);
-});
 
-describe('title', () => {
-  test('All files should be considered new', async () => {
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
-    expect(files.sort()).toEqual(SORTED_FILES);
+  describe(title, () => {
+    test('All files should be considered new', async () => {
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
+      expect(files.sort()).toEqual(SORTED_FILES);
+    });
+    test('Nothing new', async () => {
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
+      expect(files).toEqual([]);
+    });
+    test('Touch a file', async () => {
+      await delayAsync(1100);
+      await touchAsync(nodepath.join(tmpDir, 'dir/b.md'));
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
+      expect(files.sort()).toEqual(['dir/b.md'].sort());
+    });
+    test('Touch an irrelevant file', async () => {
+      await delayAsync(1100);
+      await touchAsync(nodepath.join(tmpDir, 'a.txt'));
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
+      expect(files).toEqual([]);
+    });
+    test('Create files', async () => {
+      await delayAsync(1100);
+      await touchAsync(nodepath.join(tmpDir, 'a.txt'));
+      await touchAsync(nodepath.join(tmpDir, 'new1.txt'));
+      await touchAsync(nodepath.join(tmpDir, 'new1.md'));
+      await touchAsync(nodepath.join(tmpDir, 'new2.md'));
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
+      expect(files.sort()).toEqual(['new1.md', 'new2.md']);
+    });
+    test('Ignore cache', async () => {
+      const files = await main.start(tmpDir, IGNORE_FILES, cacheDir, true);
+      expect(files.sort()).toEqual(SORTED_FILES_NEW);
+    });
   });
-  test('Nothing new', async () => {
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
-    expect(files).toEqual([]);
-  });
-  test('Touch a file', async () => {
-    await delayAsync(1100);
-    await touchAsync(nodepath.join(tmpDir, 'dir/b.md'));
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
-    expect(files.sort()).toEqual(['dir/b.md'].sort());
-  });
-  test('Touch an irrelevant file', async () => {
-    await delayAsync(1100);
-    await touchAsync(nodepath.join(tmpDir, 'a.txt'));
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
-    expect(files).toEqual([]);
-  });
-  test('Create files', async () => {
-    await delayAsync(1100);
-    await touchAsync(nodepath.join(tmpDir, 'a.txt'));
-    await touchAsync(nodepath.join(tmpDir, 'new1.txt'));
-    await touchAsync(nodepath.join(tmpDir, 'new1.md'));
-    await touchAsync(nodepath.join(tmpDir, 'new2.md'));
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir);
-    expect(files.sort()).toEqual(['new1.md', 'new2.md']);
-  });
-  test('Ignore cache', async () => {
-    const files = await main.start(tmpDir, IGNORE_FILES, cacheDir, true);
-    expect(files.sort()).toEqual(SORTED_FILES_NEW);
-  });
-});
+}
+
+testWithTmpDir(nodepath.relative('.', newTmpDir()), nodepath.relative('.', newTmpDir()), 'Relative root path');
+testWithTmpDir(newTmpDir(), newTmpDir(), 'Absolute root path');
